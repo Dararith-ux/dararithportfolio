42321f5411fafd50a8b9b5dda986731976bd63a0
Dararith-ux <rith066463799@gmail.com>
2025-09-11 11:45:36 +0700

    jmjm

---
+++ src/Navigation/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className='font-tektur text-pink-500 neon-glow text-3xl'>rith</h1>
+    </div>
+  )
+}
+
+export default Logo
--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import Logo from './Logo'
 const Nav = () => {
   return (
     <div className='flex justify-center items-center '>
-      <ul className=' py-5 px-10 list-unstyled text-white flex gap-10 bg-black-500/30 backdrop-blur-md border border-white/20 rounded-4xl font-semibold text-lg '> 
-        <li className='cursor-pointer hover:text-gray-500'>Home</li>
-        <li className='cursor-pointer hover:text-gray-500'>About me</li>
-        <li className='cursor-pointer hover:text-gray-500'>Academic</li>
-        <li className='cursor-pointer hover:text-gray-500'>Experience</li>
-        <li className='cursor-pointer hover:text-gray-500'>Project</li>
+      <Logo/>
+      <ul className=' py-5 px-10 list-unstyled text-white flex gap-10 font-semibold text-lg '> 
+        <li className='cursor-pointer hover:text-gray-500 hover:border-b-2'>Home</li>
+        <li className='cursor-pointer hover:text-gray-500 hover:border-b-2'>About me</li>
+        <li className='cursor-pointer hover:text-gray-500 hover:border-b-2'>Academic</li>
+        <li className='cursor-pointer hover:text-gray-500 hover:border-b-2'>Experience</li>
+        <li className='cursor-pointer hover:text-gray-500 hover:border-b-2'>Project</li>
       </ul>
     </div>
   )
