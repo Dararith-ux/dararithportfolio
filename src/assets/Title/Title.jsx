8957aee6c5cb580b709cbe28ae72dcd5643e1f48
Dararith-ux <rith066463799@gmail.com>
2025-12-20 14:12:05 +0700

    change

--- src/assets/Title/Title.jsx
+++ src/assets/Title/Title.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Title = ({title}) => {
   return (
     <div >
-      <h1 className='text-5xl bg-gradient-to-r from-purple-700 to-pink-400 bg-clip-text text-transparent font-bold text-center'>{title}</h1>
+      <h1 className='text-4xl bg-gradient-to-r from-purple-700 to-pink-400 bg-clip-text text-transparent font-bold text-center'>{title}</h1>
     </div>
   )
 }
